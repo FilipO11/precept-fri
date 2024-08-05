cd2aa47be1e4cb2a82f2aa9d8494be52ea58e8eb
filip <filip.opacic@outlook.com>
2024-08-05 14:09:56 +0200

    added threading to LS

--- LicenseServer.py
+++ LicenseServer.py
@@ -1,5 +1,5 @@
 # IMPORTS
-import datetime, os, time, base64
+import datetime, os, time, base64, threading
 from cryptography import x509
 from cryptography.fernet import Fernet
 from cryptography.exceptions import InvalidSignature
@@ -12,6 +12,112 @@ def xor_bytes(s1, s2):
         res.append(s1[i] ^ s2[i])
 
     return bytes(res)
+
+def client_thread(msg):
+    contentid, tid_enc = msg[:32], msg[32:]
+        
+    tid = sk_ls.decrypt(
+        ciphertext = tid_enc,
+        padding = padding.OAEP(
+            mgf = padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm = hashes.SHA256(),
+            label=None
+        )
+    )
+    
+    temp_pk_user = serialization.load_pem_public_key(tid[:174])
+    did = xor_bytes(tid[:174], tid[174:])
+    did = did[:32]
+    
+    # CHECK DID
+    if not(did in db):
+        print("ERROR: Device not registered!")
+        exit(1)
+    
+    # SEND (T_LS || r || {Sig_LS( H(r || T-LS || T_U) || PK_U(License) || ContentID ) || Cert_LS}_K)
+    temp_sk = ec.generate_private_key(ec.SECP256K1())
+    temp_pk = temp_sk.public_key()
+    nonce = os.urandom(32)
+    
+    shared = temp_sk.exchange(ec.ECDH(), temp_pk_user)
+    digest = hashes.Hash(hashes.SHA256())
+    digest.update(shared + nonce)
+    k = base64.urlsafe_b64encode(digest.finalize())
+    
+    digest = hashes.Hash(hashes.SHA256())
+    digest.update(nonce 
+                    + temp_pk.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
+                    + temp_pk_user.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
+                    )
+    exchange_hash = digest.finalize()
+    
+    digest = hashes.Hash(hashes.SHA256())
+    digest.update(did + lsid)
+    kid = digest.finalize()
+    
+    date = datetime.date.today().isoformat().encode("utf-8")
+    
+    other_data = bytes(32)
+    
+    license = sn.to_bytes(8, "big") + date + rule + other_data
+    sig_lic = sk_ls.sign(
+        license, 
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    license += sig_lic
+    
+    f = Fernet(k)
+    pt = exchange_hash + license + contentid + cert_ls_pem
+            
+    response = temp_pk.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo) + nonce + f.encrypt(pt)
+    
+    with open("comms/la.msg", "wb") as h:
+        h.write(response)
+    
+    print("Issued license.\nWaiting for confirmation...")
+    
+    # RECEIVE ({Sig_U( H(T_U || T_LS || License) || token )}_K), if successful INCREASE SN
+    confirmation = None
+    while confirmation == None:
+        try:
+            with open("comms/ls.msg", "rb") as h:
+                confirmation = h.read()
+        except FileNotFoundError:
+            time.sleep(1)
+            continue
+    os.remove("comms/ls.msg")
+    print("Confirmation received.")
+    pt = f.decrypt(confirmation, None)
+    confirmation_hash, token = pt[:512], pt[512:]
+    
+    try:
+        print("Checking confirmation...")
+        cert_user.public_key().verify(
+            confirmation_hash,
+            temp_pk_user.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
+            + temp_pk.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
+            + license,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        print("Confirmation verified.\nFinalizing...")
+    except InvalidSignature:
+        print("ERROR: Invalid license signature.")
+        exit(1)
+    
+    with open("token.prp", "wb") as h:
+        h.write(token)
+    
+    sn += 1
+    with open("sn.prp", "wb") as h:
+        h.write(sn.to_bytes(8, "big"))
 
 # LOAD FROM FILES
 with open("pki/sk_ls.pem", "rb") as h:
@@ -39,116 +145,16 @@ with open("rules.prp", "rb") as r:
 
 # LISTEN FOR LICENSE REQUESTS
 while True:
+    print("Waiting for license request...")
     try:
-        print("Waiting for license request...")
         # RECEIVE (TID || ContentID)
         with open("comms/ls.msg", "rb") as h:
             msg = h.read()
         os.remove("comms/ls.msg")
-        contentid, tid_enc = msg[:32], msg[32:]
-        
-        tid = sk_ls.decrypt(
-            ciphertext = tid_enc,
-            padding = padding.OAEP(
-                mgf = padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm = hashes.SHA256(),
-                label=None
-            )
-        )
-        
-        temp_pk_user = serialization.load_pem_public_key(tid[:174])
-        did = xor_bytes(tid[:174], tid[174:])
-        did = did[:32]
-        
-        # CHECK DID
-        if not(did in db):
-            print("ERROR: Device not registered!")
-            exit(1)
-        
-        # SEND (T_LS || r || {Sig_LS( H(r || T-LS || T_U) || PK_U(License) || ContentID ) || Cert_LS}_K)
-        temp_sk = ec.generate_private_key(ec.SECP256K1())
-        temp_pk = temp_sk.public_key()
-        nonce = os.urandom(32)
-        
-        shared = temp_sk.exchange(ec.ECDH(), temp_pk_user)
-        digest = hashes.Hash(hashes.SHA256())
-        digest.update(shared + nonce)
-        k = base64.urlsafe_b64encode(digest.finalize())
-        
-        digest = hashes.Hash(hashes.SHA256())
-        digest.update(nonce 
-                      + temp_pk.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
-                      + temp_pk_user.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
-                      )
-        exchange_hash = digest.finalize()
-        
-        digest = hashes.Hash(hashes.SHA256())
-        digest.update(did + lsid)
-        kid = digest.finalize()
-        
-        date = datetime.date.today().isoformat().encode("utf-8")
-        
-        other_data = bytes(32)
-        
-        license = sn.to_bytes(8, "big") + date + rule + other_data
-        sig_lic = sk_ls.sign(
-            license, 
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-            hashes.SHA256()
-        )
-        license += sig_lic
-        
-        f = Fernet(k)
-        pt = exchange_hash + license + contentid + cert_ls_pem
-                
-        response = temp_pk.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo) + nonce + f.encrypt(pt)
-        
-        with open("comms/la.msg", "wb") as h:
-            h.write(response)
-        
-        print("Issued license.\nWaiting for confirmation...")
-        
-        # RECEIVE ({Sig_U( H(T_U || T_LS || License) || token )}_K), if successful INCREASE SN
-        confirmation = None
-        while confirmation == None:
-            try:
-                with open("comms/ls.msg", "rb") as h:
-                    confirmation = h.read()
-            except FileNotFoundError:
-                time.sleep(1)
-                continue
-        os.remove("comms/ls.msg")
-        print("Confirmation received.")
-        pt = f.decrypt(confirmation, None)
-        confirmation_hash, token = pt[:512], pt[512:]
-        
-        try:
-            print("Checking confirmation...")
-            cert_user.public_key().verify(
-                confirmation_hash,
-                temp_pk_user.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
-                + temp_pk.public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo)
-                + license,
-                padding.PSS(
-                    mgf=padding.MGF1(hashes.SHA256()),
-                    salt_length=padding.PSS.MAX_LENGTH
-                ),
-                hashes.SHA256()
-            )
-            print("Confirmation verified.\nFinalizing...")
-        except InvalidSignature:
-            print("ERROR: Invalid license signature.")
-            exit(1)
-        
-        with open("token.prp", "wb") as h:
-            h.write(token)
-        
-        sn += 1
-        with open("sn.prp", "wb") as h:
-            h.write(sn.to_bytes(8, "big"))
+        print("License request received.\nPreparing response...")
+        thread = threading.Thread(target=client_thread, args=(msg))
+        thread.daemon = True
+        thread.start()
         
     except FileNotFoundError:
         time.sleep(2)
